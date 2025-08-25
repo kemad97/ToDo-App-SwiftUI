659ea3bc0630b847ba570688a15ce2c3af15e13f
kemad97 <90790284+kemad97@users.noreply.github.com>
2025-08-25 14:23:38 +0300

    Initial Commit

---
+++ ToDo App SwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ToDo App SwiftUI
+//
+//  Created by Kerolos on 25/08/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
